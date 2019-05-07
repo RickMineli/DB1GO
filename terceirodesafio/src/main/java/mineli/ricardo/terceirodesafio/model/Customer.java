8475f92fe9d997dd9a57982e66a5767aba9ab859
Ricardo Mineli <rick_mineli@hotmail.com>
2019-05-07 11:38:17 -0300

    Criado classe customer

---
+++ terceirodesafio/src/main/java/mineli/ricardo/terceirodesafio/model/Customer.java
@@ -0,0 +1,63 @@
+package mineli.ricardo.terceirodesafio.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phoneNumber", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    protected Customer() {
+    }
+
+    public Customer(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id.equals(customer.id) &&
+                name.equals(customer.name) &&
+                phoneNumber.equals(customer.phoneNumber) &&
+                email.equals(customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
